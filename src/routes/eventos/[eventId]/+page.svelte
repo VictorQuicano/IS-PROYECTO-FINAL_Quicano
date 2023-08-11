dd6c2a25e952118846e5be2e2ce77eeeed032e79
Alejandro <alejandroxd25k@gmail.com>
2023-08-10 23:54:55 -0500

    Cambio vistasde eventos (Básico)

--- src/routes/eventos/[eventId]/+page.svelte
+++ src/routes/eventos/[eventId]/+page.svelte
@@ -1,1 +1,11 @@
-<h1>HOLA</h1>
+<script lang="ts">
+    export let data;
+</script>
+<div>
+    <h1>
+        {data.event.titulo}
+    </h1>
+    <h2>
+        {data.event.descripcion}
+    </h2>
+</div>

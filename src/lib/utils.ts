1a3f00fd342e39d2fcc09dad2bc8b236781ff63a
Alejandro <alejandroxd25k@gmail.com>
2023-08-11 08:46:23 -0500

    funcion obtener url de imagenes de la BD

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
 export const serializeNonPOJOs = (obj: Object) => {
   return structuredClone(obj);
 };
+
+export const getURLImagen = (tablaID, registroId, nombreArchivo, tamano = '80x80') => {
+	return `https://eventos-cs.pockethost.io/api/files/${tablaID}/${registroId}/${nombreArchivo}?thumb=${tamano}`;
+};

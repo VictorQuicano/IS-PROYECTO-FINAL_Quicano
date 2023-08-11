b76b18d5c880749b5cdebf613f4d070da594dba4
Alejandro <alejandroxd25k@gmail.com>
2023-08-10 23:54:11 -0500

    Cambio en form de registro y diseño básico

--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -1,6 +1,17 @@
 <script>
   import { enhance } from "$app/forms";
   let loading = false;
+  let contra="";
+  let contraConfirmada="";
+  
+  function handle(e){
+    const target = e.target;
+    contra = e.target.value;
+  }
+  function handleConfirm(e){
+    const target = e.target;
+    contraConfirmada = e.target.value;
+  }
 
   const submitRegister = () => {
     loading = true;
@@ -25,14 +36,16 @@
       loading = false;
     };
   };
+  
+
 </script>
 
 <div class="container">
   <h1>Registro de Usuarios</h1>
   <form action="?/register" method="POST" use:enhance={submitRegister}>
     <div class="form-group">
-      <label for="username">Username</label>
-      <input name="username" placeholder="Username" />
+      <label for="username">Nombre de Usuario</label>
+      <input name="username" placeholder="Nombre de usuario" />
     </div>
     <div class="form-group">
       <label for="email">Email</label>
@@ -40,15 +53,29 @@
     </div>
     <div class="form-group">
       <label for="password">Password</label>
-      <input name="password" placeholder="Password" type="password" />
+      <input name="password" placeholder="Password" type="password" on:keyup={handle}/>
     </div>
     <div class="form-group">
       <label for="passwordConfirm">Confirm password</label>
-      <input name="passwordConfirm" placeholder="Confirm password" type="password" />
+      <input name="passwordConfirm" placeholder="Confirm password" type="password"on:keyup={handleConfirm} />
+      {#if contra != contraConfirmada}
+        <p id="advertencia">Contrasenas no coinciden</p>
+      {/if}
     </div>
     <div class="form-group">
-      <label for="name">Name</label>
-      <input name="name" placeholder="Name" />
+      <label for="nombre">Nombre</label>
+      <input name="nombre" placeholder="Nombres" />
+    </div>
+    <div class="form-group">
+      <label for="apellidos">Apellidos</label>
+      <input name="apellidos" placeholder="Apellidos" />
+    </div>
+    <div class="form-group">
+      <label for="tipoDeUsuario">¿Qué tipo de usuario es?:</label>
+        <select name="tipoDeUsuario" id="tipoDeUsuario">
+          <option value="Alumno">Alumno</option>
+          <option value="Docente">Docente</option>
+        </select>
     </div>
     <button type="submit" disabled={loading}>Register</button>
   </form>
@@ -60,7 +87,20 @@
     margin: 0;
     font-family: Arial, sans-serif;
   }
-
+  .form-group label{
+    padding-bottom: 5px;
+  }
+  select{
+    width: 45%;
+    padding: 5px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    place-content: right;    
+  }
+  #advertencia{
+    color: red;
+    font-size: 9px;
+  }
   /* Estilos del contenedor */
   .container {
     width: 400px;
@@ -86,7 +126,7 @@
 
   /* Estilos de los grupos de campos */
   .form-group {
-    margin-bottom: 20px;
+    margin-bottom: 15px;
   }
 
   label {
@@ -97,6 +137,7 @@
   }
 
   input {
+    margin-top: 10px;
     width: 100%;
     padding: 12px;
     border: 1px solid #ccc;
